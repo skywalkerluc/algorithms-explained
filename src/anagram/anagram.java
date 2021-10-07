9a0a08a10c0bdec9d31320cdbade699301b4ff5e
Shivam Choudhary <53565563+ShivamChoudhary17@users.noreply.github.com>
2021-10-07 20:14:45 +0530

    Update anagram.java

--- src/anagram/anagram.java
+++ src/anagram/anagram.java
@@ -1,4 +1,32 @@
- public List<List<String>> groupAnagramsCategorizeByFrequency(String[] strs) {
+ import java.util.*;
+
+class GroupAnagrams {
+
+  public List<List<String>> groupAnagramsCategorizeBySorting(String[] strs) {
+
+    if (strs == null || strs.length == 0)
+      return new ArrayList<>();
+
+    Map<String, List<String>> stringAnagramsMap = new HashMap<>();
+    for (String s : strs) {
+      char[] arr = s.toCharArray();
+      Arrays.sort(arr);
+      String key = String.valueOf(arr);
+
+      if (!stringAnagramsMap.containsKey(key))
+        stringAnagramsMap.put(key, new ArrayList<>());
+
+      stringAnagramsMap.get(key).add(s);
+    }
+
+    List<List<String>> resultList = new ArrayList<>();
+    for (Map.Entry<String, List<String>> stringAnagrams : stringAnagramsMap.entrySet()) {
+      resultList.add(stringAnagrams.getValue());
+    }
+    return resultList;
+  }
+
+  public List<List<String>> groupAnagramsCategorizeByFrequency(String[] strs) {
 
     // Check for empty inputs
     if (strs == null || strs.length == 0)
@@ -31,5 +59,18 @@
 
     // Iterate over each character
     for (char c : str.toCharArray()) {
-      freq++;
+      freq[c - 'a']++;
     }
+
+    // Start creating the frequency string
+    StringBuilder frequencyString = new StringBuilder("");
+    char c = 'a';
+    for (int i : freq) {
+      frequencyString.append(c);
+      frequencyString.append(i);
+      c++;
+    }
+
+    return frequencyString.toString();
+  }
+}
